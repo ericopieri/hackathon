957512d6d873f654bae516d7de7f02173761ddfa
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-06-16 17:00:03 -0300

    inicio com o fabricjs

--- src/components/Canva.vue
+++ src/components/Canva.vue
@@ -1,10 +1,21 @@
 <template>
-  <canvas :class="`canva-${$route.params.id}`"></canvas>
+  <canvas :class="`canva-${$route.params.id}`" :id="`canva-${$route.params.id}`"></canvas>
 </template>
 
 <script>
+import { fabric } from 'fabric'
 export default {
-
+    mounted(){
+        var canvas = new fabric.Canvas(`canva-${this.$route.params.id}`);
+        var rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 20
+        });
+        canvas.add(rect);
+    }
 }
 </script>
 
